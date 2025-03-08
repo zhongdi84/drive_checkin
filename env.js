797d5fb4adf7decacfb6f810692d1d2697198775
zhongdi84 <zhongdi84@qq.com>
2025-03-08 12:59:17 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     private_only_first: process.env.PRIVATE_ONLY_FIRST || false,
-    private_threadx: process.env.PRIVATE_THREADX || 10,
-    family_threadx: process.env.FAMILY_THREADX || 10,
+    private_threadx: process.env.PRIVATE_THREADX || 30,
+    family_threadx: process.env.FAMILY_THREADX || 30,
     tyys: process.env.TYYS || "",
     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID || "",
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || "",
